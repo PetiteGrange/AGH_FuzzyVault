74364d95390f3a35df50b95f50433614af7879f9
PetiteGrange <alexis.heuschling2@gmail.com>
2025-01-29 11:53:42 +0100

    Add generate_secret to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,9 @@
+import random
 import hashlib
+import string
+import nltk
+from nltk.corpus import words
+from src import *
 
 def hash_word_to_number(word, mod=1000):
     """
@@ -26,3 +31,20 @@ def split_text(text, chunk_size=6):
         list of str: Chunks of the secret.
     """
     return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+
+
+
+def generate_secret(length=20):
+    """
+    Generate a random secret to be stored in the fuzzy vault
+
+    Args:
+        length (int): The length of the secret, default to 20
+    
+    Returns:
+        str: the secret
+
+    """
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
